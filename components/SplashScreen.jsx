3d248772208aba863914e29c5b8a43c9a8c3ac72
Poyraz Avsever <poyrazavsever@hotmail.com>
2024-09-20 11:49:57 +0300

    feat: updated splash screen

--- components/SplashScreen.jsx
+++ components/SplashScreen.jsx
@@ -15,48 +15,47 @@ function SplashScreen({ finishLoading }) {
             delay: 0,
             translateY: [100, 0],
             opacity: [0, .5],
-            duration: 500,
-            easing: "easeInOutExpo"
-
+            duration: 300, // Süreyi kısalttım
+            easing: "easeInOutQuad" // Daha hızlı easing fonksiyonu
         })
         loader.add({
             targets: "#title",
-            delay:3,
+            delay: 100, // Gecikmeyi düşürdüm
             translateY: [-100, 0],
             opacity: [0, 1],
-            easing: "easeInOutExpo"
-
+            duration: 400, // Süreyi kısalttım
+            easing: "easeInOutQuad"
         })
-
         loader.add({
             targets: '#dot',
-            delay: 4,
+            delay: 200, // Gecikmeyi düşürdüm
             translateY: [-500, 0],
             opacity: [0, 1],
-            easing: "easeInOutExpo"
+            duration: 300, // Süreyi kısalttım
+            easing: "easeInOutQuad"
         })
         loader.add({
             targets: "#shadow",
-            delay: 3,
+            delay: 100, // Gecikmeyi düşürdüm
             translateY: [0, -100],
             opacity: [.5, 0],
-            easing: "easeInOutExpo"
-
+            duration: 300, // Süreyi kısalttım
+            easing: "easeInOutQuad"
         })
         loader.add({
             targets: "#title",
-            delay:3,
+            delay: 100, // Gecikmeyi düşürdüm
             translateY: [0, -100],
             opacity: [1, 0],
-            easing: "easeInOutExpo"
-
+            duration: 400, // Süreyi kısalttım
+            easing: "easeInOutQuad"
         })
-
         loader.add({
             targets: '#dot',
-            delay: 4,
+            delay: 200, // Gecikmeyi düşürdüm
             opacity: [1, 0],
-            easing: "easeInOutExpo"
+            duration: 300, // Süreyi kısalttım
+            easing: "easeInOutQuad"
         })
     }
 
@@ -71,7 +70,6 @@ function SplashScreen({ finishLoading }) {
             <div className='relative'>
                 <div id="shadow" className='absolute -top-8 -left-24 w-36 h-36 bg-violet-600 opacity-30 blur-3xl !-z-40'></div>
                 <h2 id='title' className='text-2xl text-neutral-50 sm:text-4xl md:text-6xl font-bold'>Ohrama <span id='dot' className='text-violet-700'>.</span></h2>
-                
             </div>
         </div>
     )
